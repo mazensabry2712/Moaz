da2456a5f8eee251b117562bb4a069af935e862e
mazensabry2712 <mazensoftware30@gmail.com>
2025-05-15 18:16:05 +0300

    first commit

---
+++ intro.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+// using namespace std;
+int main() {
+    std::cout << "Hello, VS Code!\n\n\n";
+
+    std::cout << "Hello, VS Code!"<< std::endl;
+
+    std::cout << "Hello, VS Code!\n";
+
+    std::cout << "Hello, VS Code!"<<std::endl;
+    /*-- MAZEN PRINT --*/ 
+    std::cout << "mazen     "<<"Mazen " << "MaZen" << std::endl;
+  /*--// MAZEN PRINT --*/ 
+    std::cout << "The result of: " << "8+4 = " << 8+4<< std::endl;
+
+
+
+    // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    // =>single line comment
+    /*
+    multi line comment
+    */
+    
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+std::cout<< "m1\  m2\  m3\  m4\n";
+std::cout<< "m1\\m2\\m3\\m4";
+    return 0;
+}
